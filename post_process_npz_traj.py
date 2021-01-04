6d42c6d9a796b42cad4c1eda015959a314b7c69e
Médéric Fourmy <mfourmy@laas.fr>
2021-01-04 16:43:57 +0100

    Last modifs postprocess and solo_filters

--- post_process_npz_traj.py
+++ post_process_npz_traj.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 import numpy as np
@@ -18,7 +21,6 @@ from matplotlib.collections import LineCollection
 from data_readers import shortened_arr_dic
 
 cwdir = os.getcwd()
-print(cwdir)
 DATA_FOLDER_RESULTS = os.path.join(cwdir, 'data/quadruped_experiments_results/')
 print(DATA_FOLDER_RESULTS)
 
@@ -29,134 +31,121 @@ data_file_post = 'out_post.npz'
 # Keys:
 print('Reading ', DATA_FOLDER_RESULTS+data_file)
 arr_dic = np.load(DATA_FOLDER_RESULTS+data_file)
-arr_dic = shortened_arr_dic(arr_dic, 0, 10000)
+N = len(arr_dic['t'])
+arr_dic = shortened_arr_dic(arr_dic, 0, N-200)
 
 dt = 1e-3
 t_arr = arr_dic['t']
 N = len(t_arr)
 print('N: ', N)
+
+# EST
+q_kf_arr = arr_dic['q_kf']
+v_kf_arr = arr_dic['v_kf']
+q_cf_arr = arr_dic['q_cf']
+v_cf_arr = arr_dic['v_cf']
+
+o_p_ob_kf_arr = q_kf_arr[:,:3]
+o_p_ob_cf_arr = q_cf_arr[:,:3]
+o_q_b_kf_arr = q_kf_arr[:,3:7]
+o_q_b_cf_arr = q_cf_arr[:,3:7]
+
+b_v_ob_kf_arr = v_kf_arr[:,:3]
+b_v_ob_cf_arr = v_cf_arr[:,:3]
+
 # GT
 w_p_wm_arr = arr_dic['w_p_wm']
 w_q_m_arr = arr_dic['w_q_m']
 w_v_wm_arr = arr_dic['w_v_wm']
 m_v_wm_arr = arr_dic['m_v_wm']
 
-q_kf_arr = arr_dic['q_kf']
-v_kf_arr = arr_dic['v_kf']
-q_cf_arr = arr_dic['q_cf']
-v_cf_arr = arr_dic['v_cf']
+# plot before trajectory alignment
+plt.figure('P KF CF before alignment')
+plt.plot(w_p_wm_arr[:,0], w_p_wm_arr[:,1], label='MOCAP')
+plt.plot(q_kf_arr[:,0], q_kf_arr[:,1], label='KF')
+plt.plot(q_cf_arr[:,0], q_cf_arr[:,1], label='CF')
+plt.legend()
+plt.grid()
+# plt.show()
 
-b_v_ob_kf_arr = v_kf_arr[:,:3]
-b_v_ob_cf_arr = v_cf_arr[:,:3]
+# Trajectory alignment 
+# based on first frame (se3 alignment)
+o_p_ob_kf_init = o_p_ob_kf_arr[0,:]
+o_p_ob_cf_init = o_p_ob_cf_arr[0,:]
+w_p_wm_init = w_p_wm_arr[0,:]
 
+w_R_m_init = pin.Quaternion(w_q_m_arr[0,:].reshape((4,1))).toRotationMatrix()
+o_R_b_kf_init = pin.Quaternion(o_q_b_kf_arr[0,:].reshape((4,1))).toRotationMatrix()
+o_R_b_cf_init = pin.Quaternion(o_q_b_cf_arr[0,:].reshape((4,1))).toRotationMatrix()
 
+w_T_m_init = pin.SE3(w_R_m_init, w_p_wm_init)
+o_T_b_kf_init = pin.SE3(o_R_b_kf_init, o_p_ob_kf_init)
+o_T_b_cf_init = pin.SE3(o_R_b_cf_init, o_p_ob_cf_init)
+
+w_T_okf = w_T_m_init * o_T_b_kf_init.inverse()
+w_T_ocf = w_T_m_init * o_T_b_cf_init.inverse()
+
+# transform estimated trajectories in mocap frame
+w_p_wb_kf_arr = np.array([w_T_okf.act(o_p_ob_kf) for o_p_ob_kf in o_p_ob_kf_arr])
+w_p_wb_cf_arr = np.array([w_T_ocf.act(o_p_ob_cf) for o_p_ob_cf in o_p_ob_cf_arr])
+
+# TODO: same for orientations
 
 #####################################
 # MOCAP freq 200Hz vs robot freq 1kHz
 NB_OVER = 5
-
 # compute filterd Mo-Cap velocity
 w_p_wm_arr_sub = w_p_wm_arr[::NB_OVER]
 w_v_wm_arr_sagol_sub = signal.savgol_filter(w_p_wm_arr_sub, window_length=21, polyorder=3, deriv=1, axis=0, delta=NB_OVER*dt, mode='mirror')
 w_v_wm_arr_sagol = w_v_wm_arr_sagol_sub.repeat(NB_OVER, axis=0) 
-
-# compute base velocities
+# compute mocap base velocities
 w_R_m_lst = [pin.Quaternion(w_q_m.reshape((4,1))).toRotationMatrix() for w_q_m in w_q_m_arr] 
 m_v_wm_arr_filt = np.array([w_R_m.T @ w_v_wm for w_R_m, w_v_wm in zip(w_R_m_lst, w_v_wm_arr_sagol)])
+print(m_v_wm_arr_filt.shape)
 
-
-# #####################
-# # Trajectory alignment
-# pose_est = np.hstack([t_arr.reshape((N,1)), o_p_ob_arr, o_q_b_arr])
-# pose_gtr = np.hstack([t_arr.reshape((N,1)), w_p_wm_arr, w_q_m_arr])
-
-# # filter out end of traj where the robot solo causes strange jump in Mo-Cap position
-# # NPREV = 12
-# # t_arr = t_arr[:N-NPREV] 
-# # pose_est = pose_est[:N-NPREV,:]
-# # pose_gtr = pose_gtr[:N-NPREV,:]
-# # m_v_wm_arr_filt = m_v_wm_arr_filt[:N-NPREV,:]
-# # b_v_ob_arr = b_v_ob_arr[:N-NPREV,:]
-# # b_v_oc_arr = b_v_oc_arr[:N-NPREV,:]
-
-# res_folder = 'res_for_rpg_'+datetime.datetime.now().strftime("%y_%m_%d__%H_%M_%S") + '/'
-# os.makedirs(res_folder)
-# np.savetxt(res_folder+'stamped_traj_estimate.txt', pose_est, delimiter=' ')
-# np.savetxt(res_folder+'stamped_groundtruth.txt',   pose_gtr, delimiter=' ')
-
-# traj = rpg_traj.Trajectory(res_folder, align_type='se3', align_num_frames=1)  # settings ensured by eval_cfg.yaml
-# # 'a' like aligned
-# a_p_ab_arr = traj.p_es_aligned
-# a_q_b_arr = traj.q_es_aligned
-
-# # compute the relative transform applied to the estimation trajectory and propagate it to center of mass quantities
-
-# a_T_b0 = pin.SE3(pin.Quaternion(a_q_b_arr[0,:].reshape((4,1))).toRotationMatrix(), a_p_ab_arr[0,:])
-# o_T_b0 = pin.SE3(pin.Quaternion(o_q_b_arr[0,:].reshape((4,1))).toRotationMatrix(), o_p_ob_arr[0,:])
-# a_T_o = a_T_b0 * o_T_b0.inverse()  # compute alignment transformation based on the first frame
-# a_R_o = a_T_o.rotation
-
-# # align CoM trajectory
-# a_p_ac_arr = [a_T_o*o_p_oc for o_p_oc in o_p_oc_arr]
-# a_v_ac_arr = [a_R_o*o_v_oc for o_v_oc in o_v_oc_arr]
-# a_L_arr = [a_R_o@o_Lc for o_Lc in o_Lc_arr]
-
-
-# # save in a new file all needed things for display
-# keys_to_keep = ['t', 'w_p_wm', 'w_q_m', 'qa']
-# arr_dic_post = {k: arr_dic[k] for k in keys_to_keep}
-# arr_dic_post['a_p_ab'] = a_p_ab_arr
-# arr_dic_post['a_q_b'] = a_q_b_arr
-# arr_dic_post['a_p_ac'] = a_p_ac_arr 
-# arr_dic_post['a_L'] = a_L_arr 
-
-# print('Saving ', DATA_FOLDER_RESULTS+data_file_post)
-# np.savez(DATA_FOLDER_RESULTS+data_file_post, **arr_dic_post)
-
-
-
-# traj.compute_absolute_error()
-# traj.compute_boxplot_distances()
-# traj.compute_relative_errors()
-# traj.compute_relative_error_at_subtraj_len()
 
 # Create a continuous norm to map from data points to colors
+def plot_xy_traj(xy, cmap, fig, ax, cstart):
+    points = xy.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    norm = plt.Normalize(t_arr[0], t_arr[-1])
+    lc = LineCollection(segments, cmap=cmap, norm=norm)
+    # Set the values used for colormapping
+    lc.set_array(t_arr)
+    lc.set_linewidth(2)
+    line = ax.add_collection(lc)
+    # fig.colorbar(line, ax=ax)
+    plt.plot(xy[0,0], xy[0,1], cstart+'x')
 
-# def plot_xy_traj(xy, cmap, fig, ax, cstart):
-#     points = xy.reshape(-1, 1, 2)
-#     segments = np.concatenate([points[:-1], points[1:]], axis=1)
-#     norm = plt.Normalize(t_arr[0], t_arr[-1])
-#     lc = LineCollection(segments, cmap=cmap, norm=norm)
-#     # Set the values used for colormapping
-#     lc.set_array(t_arr)
-#     lc.set_linewidth(2)
-#     line = ax.add_collection(lc)
-#     # fig.colorbar(line, ax=ax)
-#     plt.plot(xy[0,0], xy[0,1], cstart+'x')
 
+fig, ax = plt.subplots(1, 1, sharex=True, sharey=True)
+plot_xy_traj(w_p_wm_arr[:,:2], 'winter', fig, ax, 'b')
+plot_xy_traj(w_p_wb_cf_arr[:,:2], 'autumn', fig, ax, 'r')
+plot_xy_traj(w_p_wb_kf_arr[:,:2], 'spring', fig, ax, 'r')
+fig.tight_layout()
 
-# fig, ax = plt.subplots(1, 1, sharex=True, sharey=True)
-# plot_xy_traj(traj.p_es_aligned[:,:2], 'autumn', fig, ax, 'b')
-# plot_xy_traj(traj.p_gt[:,:2], 'winter', fig, ax, 'r')
-# fig.tight_layout()
+xmin = w_p_wm_arr[:,0].min() 
+xmax = w_p_wm_arr[:,0].max() 
+ymin = w_p_wm_arr[:,1].min() 
+ymax = w_p_wm_arr[:,1].max()
 
-# xmin = traj.p_es_aligned[:,0].min() 
-# xmax = traj.p_es_aligned[:,0].max() 
-# ymin = traj.p_es_aligned[:,1].min() 
-# ymax = traj.p_es_aligned[:,1].max()
+xmin = min(xmin, w_p_wb_cf_arr[:,0].min()) 
+xmax = max(xmax, w_p_wb_cf_arr[:,0].max()) 
+ymin = min(ymin, w_p_wb_cf_arr[:,1].min()) 
+ymax = max(ymax, w_p_wb_cf_arr[:,1].max())
 
-# xmin = min(xmin, traj.p_gt[:,0].min()) 
-# xmax = max(xmax, traj.p_gt[:,0].max()) 
-# ymin = min(ymin, traj.p_gt[:,1].min()) 
-# ymax = max(ymax, traj.p_gt[:,1].max())
+xmin = min(xmin, w_p_wb_kf_arr[:,0].min()) 
+xmax = max(xmax, w_p_wb_kf_arr[:,0].max()) 
+ymin = min(ymin, w_p_wb_kf_arr[:,1].min()) 
+ymax = max(ymax, w_p_wb_kf_arr[:,1].max())
 
-# offx = 0.1*(xmax - xmin)
-# offy = 0.1*(ymax - ymin)
+offx = 0.1*(xmax - xmin)
+offy = 0.1*(ymax - ymin)
 
-# # line collections don't auto-scale the plot
-# plt.xlim(xmin-offx, xmax+offx) 
-# plt.ylim(ymin-offy, ymax+offy)
-# plt.grid()
+# line collections don't auto-scale the plot
+plt.xlim(xmin-offx, xmax+offx) 
+plt.ylim(ymin-offy, ymax+offy)
+plt.grid()
 
 
 
@@ -193,6 +182,7 @@ EXT = '.pdf'
 
 fig, axs = plt.subplots(3,1, figsize=FIGSIZE)
 fig.canvas.set_window_title('base_velocity_base_frame')
+axs[0].set_title('base_velocity_base_frame')
 ylabels = ['Vx [m/s]', 'Vy [m/s]', 'Vz [m/s]']
 for i in range(3):
     axs[i].plot(t_arr, b_v_ob_kf_arr[:,i], markersize=1, label='KF')
@@ -204,17 +194,19 @@ for i in range(3):
 axs[2].set_xlabel('time [s]')
 axs[0].legend()
 
-# fig, axs = plt.subplots(3,1, figsize=FIGSIZE)
-# fig.canvas.set_window_title('base_position')
-# ylabels = ['Px [m]', 'Py [m]', 'Pz [m]']
-# for i in range(3):
-#     axs[i].plot(t_arr, traj.p_es_aligned[:,i], 'b', markersize=1, label='est')
-#     axs[i].plot(t_arr, traj.p_gt[:,i], 'r', markersize=1, label='Mo-Cap')
-#     axs[i].set_ylabel(ylabels[i])
-#     axs[i].yaxis.set_label_position("right")
-#     axs[i].grid(GRID)
-# axs[2].set_xlabel('time [s]')
-# axs[0].legend()
+fig, axs = plt.subplots(3,1, figsize=FIGSIZE)
+fig.canvas.set_window_title('base_position')
+axs[0].set_title('base_position')
+ylabels = ['Px [m]', 'Py [m]', 'Pz [m]']
+for i in range(3):
+    axs[i].plot(t_arr, w_p_wb_kf_arr[:,i], markersize=1, label='KF')
+    axs[i].plot(t_arr, w_p_wb_cf_arr[:,i], markersize=1, label='CF')
+    axs[i].plot(t_arr, w_p_wm_arr[:,i], markersize=1, label='Mo-Cap')
+    axs[i].set_ylabel(ylabels[i])
+    axs[i].yaxis.set_label_position("right")
+    axs[i].grid(GRID)
+axs[2].set_xlabel('time [s]')
+axs[0].legend()
 
 
 
--- solo_filters_comparison.py
+++ solo_filters_comparison.py
@@ -29,17 +29,33 @@ dt = 1e-3
 
 cwdir = os.getcwd()
 DATA_FOLDER_RESULTS = os.path.join(cwdir, 'data/quadruped_experiments_results/')
-os.makedirs(DATA_FOLDER_RESULTS)
+if not os.path.exists(DATA_FOLDER_RESULTS):
+    os.makedirs(DATA_FOLDER_RESULTS)
 DATA_FOLDER = os.path.join(cwdir, 'data/')
 
-data_file_meas = 'Logs_05_10_2020_18h/data_2020_11_05_18_18.npz'
-data_file_ctrl = 'Logs_05_10_2020_18h/data_control_2020_11_05_18_18.npz'
+# data_file_meas = 'Logs_05_10_2020_18h/data_2020_11_05_18_18.npz'
+# data_file_ctrl = 'Logs_05_10_2020_18h/data_control_2020_11_05_18_18.npz'
+
+
+# data_file_meas = 'Experiments_Replay_30_11_2020_bis/data_2020_11_30_17_17.npz'  # stamping
+# data_file_meas = 'Experiments_Replay_30_11_2020_bis/data_2020_11_30_17_18.npz'  # sin
+# data_file_meas = 'Experiments_Replay_30_11_2020_bis/data_2020_11_30_17_22.npz'  # walking
+data_file_ctrl = 'Experiments_Replay_30_11_2020_bis/data_control_2020_11_30_17_22.npz'
+
+# data_file_meas = 'Point_feet_27_11_20/data_2020_11_27_14_46.npz'
+# data_file_ctrl = 'Logs_05_10_2020_18h/data_control_2020_11_05_18_18.npz'
+
+# data_file_meas = 'SinStamping_Corrected_09_12_2020/data_2020_12_09_17_54.npz'  # sin
+data_file_meas = 'SinStamping_Corrected_09_12_2020/data_2020_12_09_17_56.npz'  # stamping
+
+CTRL_FILE = False  # use the available control file for contacts
+THRESH_FZ = 1  # sin
+
 
 print('Reading ', DATA_FOLDER+data_file_meas)
 arr_dic_meas = read_data_file_laas(DATA_FOLDER+data_file_meas, dt)
 print('Reading ', DATA_FOLDER+data_file_ctrl)
 arr_dic_ctrl = read_data_file_laas_ctrl(DATA_FOLDER+data_file_ctrl)
-# arr_dic_meas = shortened_arr_dic(arr_dic_meas, 0, 2000)
 
 t_arr = arr_dic_meas['t']
 N = len(t_arr)
@@ -50,20 +66,31 @@ N = len(t_arr)
 # initial state
 # position: 0,0,0
 # velocity: 0,0,0 -> HYP robot does not move
-o_p_oi = np.zeros(3)
+# height of the robot base frame
+# H_INIT = 0.235  # with urdf feet
+# H_INIT = 0.22294615  # with smaller black feet
+H_INIT = 0.205  # with point feet
+
+o_p_ob = np.array([0,0,H_INIT])
 o_q_i = arr_dic_meas['o_q_i'][0,:]
 o_v_oi = np.zeros(3)
 qa = arr_dic_meas['qa'][0,:]
-q_init = np.hstack([o_p_oi, o_q_i, qa])
+q_init = np.hstack([o_p_ob, o_q_i, qa])
 
 kf = ImuLegKF(dt, q_init)
-cf = Estimator(dt, N)
+cf = Estimator(dt, N, h_init=H_INIT)
+
+robot = load('solo12')
+force_est = ContactForcesEstimator(robot, kf.contact_ids, dt)
 
 # some useful recordings
-q_arr_kf = np.zeros((N, 19))
-v_arr_kf = np.zeros((N, 18))
-q_arr_cf = np.zeros((N, 19))
-v_arr_cf = np.zeros((N, 18))
+q_kf_arr = np.zeros((N, 19))
+v_kf_arr = np.zeros((N, 18))
+q_cf_arr = np.zeros((N, 19))
+v_cf_arr = np.zeros((N, 18))
+fz_arr = np.zeros((N,4))
+contact_status_arr = np.zeros((N,4))
+feet_state_arr = np.zeros((N,4*3))
 
 for i in range(N):
     # define measurements
@@ -75,17 +102,34 @@ for i in range(N):
     qa = arr_dic_meas['qa'][i,:]
     dqa = arr_dic_meas['dqa'][i,:]
 
-    contact_status = arr_dic_meas['contactStatus'][i,:]
-    goals = arr_dic_ctrl['log_feet_pos_target'][:,:,i]
+    if CTRL_FILE:
+        contact_status = arr_dic_meas['contactStatus'][i,:]
+        goals = arr_dic_ctrl['log_feet_pos_target'][:,:,i]
+    else:
+        # contact estimation based on force estimation
+        ddqa = np.zeros(12)
+        i_domg_oi = np.zeros(3)
+        tauj = arr_dic_meas['tau'][i,:]
+        o_forces = force_est.compute_contact_forces2(qa, dqa, ddqa, o_R_i, i_omg_oi, i_domg_oi, o_a_oi, tauj, world_frame=True)
+        fz_arr[i,:] = o_forces[:,2]
+        contact_status = np.array([fz > THRESH_FZ for fz in o_forces[:,2]])  # simple contact detection
+        contact_status_arr[i,:] = contact_status 
+
+        # initial blip
+        if i*dt < 0.3:
+            contact_status = np.zeros(4)
+
+        goals = np.zeros((3,4))
 
     # Kalman Filter
     kf.run_filter(o_a_oi, o_R_i, qa, dqa, i_omg_oi, contact_status)
-    q_arr_kf[i,:], v_arr_kf[i,:] = kf.get_configurations()
+    q_kf_arr[i,:], v_kf_arr[i,:] = kf.get_configurations()
+    feet_state_arr[i,:] = kf.get_state()[6:]
 
     # Complementary filter
     device = Device(o_a_oi, i_omg_oi, o_q_i, qa, dqa)
-    cf.run_filter(i, contact_status, device, goals)
-    q_arr_cf[i,:], v_arr_cf[i,:] = (conf for conf in cf.get_configurations())
+    cf.run_filter(i, contact_status, device, goals, remaining_steps=100)
+    q_cf_arr[i,:], v_cf_arr[i,:] = cf.get_configurations()
 
 
 # data to copy
@@ -94,12 +138,47 @@ copy_lst = ['t', 'w_v_wm', 'm_v_wm', 'w_q_m', 'w_p_wm']
 for k in copy_lst:
     res_arr_dic[k] = arr_dic_meas[k]
 # add estimated data
-res_arr_dic['q_kf'] = q_arr_kf
-res_arr_dic['v_kf'] = v_arr_kf
-res_arr_dic['q_cf'] = q_arr_cf
-res_arr_dic['v_cf'] = v_arr_cf
+res_arr_dic['q_kf'] = q_kf_arr
+res_arr_dic['v_kf'] = v_kf_arr
+res_arr_dic['q_cf'] = q_cf_arr
+res_arr_dic['v_cf'] = v_cf_arr
 
 
 out_path = DATA_FOLDER_RESULTS+'out.npz'
 np.savez(out_path, **res_arr_dic)
 print(out_path, ' saved')
+
+
+# some plots
+plt.figure('Normal forces')
+plt.title('Normal forces')
+for i in range(4):
+    plt.subplot(4,1,i+1)
+    plt.plot(t_arr, fz_arr[:,i], label='fz '+kf.contact_frame_names[i])
+    plt.plot(t_arr, contact_status_arr[:,i]*THRESH_FZ, label='contact')
+    plt.plot(t_arr, arr_dic_meas['contactStatus'][:,i]*THRESH_FZ, label='contact')
+    plt.hlines(0, t_arr[0]-1, t_arr[-1]+1, 'k')
+plt.legend()
+
+plt.figure('Filter positions')
+plt.title('Filter positions')
+for i in range(3):
+    plt.subplot(3,1,1+i)
+    plt.plot(t_arr, q_kf_arr[:,i], 'g', label='KF')
+    plt.plot(t_arr, q_cf_arr[:,i], 'b', label='CF')
+    plt.legend()
+
+plt.figure('KF feet')
+plt.title('KF feet')
+for i in range(3):
+    plt.subplot(3,1,1+i)
+    for j in range(4):
+        plt.plot(t_arr, feet_state_arr[:,3*j+i], label=str(j))
+    plt.legend()
+
+
+
+if '--show' in sys.argv:
+    plt.show()
+    
+
